defb8d7da22149674fc4d933b64060c34aac967e
SHARUKASH <sharukash01@gmail.com>
2026-08-03 11:06:30 +0530

    adding one category card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import CategoryCard from "./components/CategoryCard";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+
+      <CategoryCard />
     </div>
   );
 }
---
+++ client/src/components/CategoryCard.jsx
@@ -0,0 +1,13 @@
+import "./CategoryCard.css";
+
+function CategoryCard() {
+  return (
+    <div className="category-card">
+      <span className="emoji">🍕</span>
+      <h3>Pizza</h3>
+    </div>
+  );
+}
+
+export default CategoryCard;
+
